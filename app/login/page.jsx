fbf466bf77dc0568457410a099e7b2a0f3c4600e
ayush <ayushkr347@gmail.com>
2025-07-26 19:13:30 +0530

    modified

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -27,34 +27,38 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="max-w-sm mx-auto mt-20 p-4 border rounded shadow-2xl dark:bg-gray-900 bg-[#EFE4D2]">
-      <h2 className="text-xl font-bold mb-4">Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-        type="email"
-          placeholder="Email"
-          className="w-full p-2 border mb-2 text-black dark-text-black"
-          value={form.email}
-          onChange={(e) => setForm({ ...form, email: e.target.value })}
-        />
-        <input
-          placeholder="Password"
-          type="password"
-          className="w-full p-2 border mb-4 text-black dark-text-black"
-          value={form.password}
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
-        />
-        <button className="bg-[#442410] dark:bg-blue-600 text-white px-4 py-2 rounded w-full">
-          Login
-        </button>
-        <p className="text-sm mt-4 text-center">
-          Don't have an account?{" "}
-          <a href="/signup" className="text-blue-600">
-            Signup
-          </a>
-        </p>
-        {error && <p className="text-red-700 mt-2">{error}</p>}
-      </form>
-    </div>
+    <>
+      <h2 className="text-3xl font-bold mb-4 text-center text-[#442410] dark:text-white">
+        Login
+      </h2>
+      <div className="max-w-md mx-auto mt-10 p-4 rounded-2xl shadow-2xl dark:bg-gray-700 bg-[#FAF5EF] ">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full p-2 border mb-2 text-black dark-text-black rounded-xl mt-4"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            className="w-full p-2 border mb-4 text-black dark-text-black rounded-xl mt-4 "
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+          />
+          <button className="bg-[#442410] dark:bg-blue-600 text-white px-4 py-2 mt-4 w-full rounded-xl">
+            Login
+          </button>
+          <p className="text-sm mt-4 text-center">
+            Don't have an account?{" "}
+            <a href="/signup" className="text-blue-600">
+              Signup
+            </a>
+          </p>
+          {error && <p className="text-red-700 mt-2 text-center">{error}</p>}
+        </form>
+      </div>
+    </>
   );
 }
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../context/AuthContext";
 
@@ -10,14 +10,24 @@ export default function SignupPage() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
 
-  
+useEffect(() => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme) {
+    document.documentElement.classList.toggle("dark", savedTheme === "dark");
+  }
+}, []);
+
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (!form.name || !form.email || !form.password) {
       setError("All fields are required.");
-      setMessage("");
+      return;
+    }
+    if (form.password.length <= 6) {
+      
+      setError("Minimum password length should be 6");
       return;
     }
 
@@ -38,41 +48,45 @@ export default function SignupPage() {
   };
 
   return (
-    <div className="max-w-sm mx-auto mt-20 p-4 border rounded shadow-2xl bg-[#EFE4D2] dark:bg-gray-900">
-      <h2 className="text-xl font-bold mb-4 ">Signup</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Name"
-          className="w-full p-2 border mb-2 text-black dark-text-black"
-          value={form.name}
-          onChange={(e) => setForm({ ...form, name: e.target.value })}
-        />
-        <input
-        type="email"
-          placeholder="Email"
-          className="w-full p-2 border mb-2 text-black dark-text-black"
-          value={form.email}
-          onChange={(e) => setForm({ ...form, email: e.target.value })}
-        />
-        <input
-          placeholder="Password"
-          type="password"
-          className="w-full p-2 border mb-4 text-black dark-text-black"
-          value={form.password}
-          onChange={(e) => setForm({ ...form, password: e.target.value })}
-        />
-        <button className=" text-white px-4 py-2 rounded w-full bg-[#442410] dark:bg-blue-600">
-          Signup
-        </button>
-        <p className="text-sm mt-4 text-center">
-          Already have an account?{" "}
-          <a href="/login" className="text-blue-600">
-            Login
-          </a>
-        </p>
-        {error && <p className="text-red-600 mt-2">{error}</p>}
-        {message && <p className="text-green-700 mt-2">{message}</p>}
-      </form>
-    </div>
+    <>
+      <h2 className="text-3xl font-bold mb-4 text-center text-[#442410] dark:text-white">
+        Signup
+      </h2>
+      <div className="max-w-lg  mx-auto mt-10 p-8  rounded-2xl shadow-2xl bg-[#FAF5EF]  dark:bg-gray-700">
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Name"
+            className="w-full p-2 border mb-2 text-black dark-text-black rounded-xl mt-2"
+            value={form.name}
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full p-2 border mb-2 text-black dark-text-black rounded-xl mt-2"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            className="w-full p-2 border mb-4 text-black dark-text-black rounded-xl mt-2"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+          />
+          <button className=" text-white px-4 py-2  w-full bg-[#442410] dark:bg-blue-600 rounded-xl mt-2">
+            Signup
+          </button>
+          <p className="text-sm mt-4 text-center">
+            Already have an account?{" "}
+            <a href="/login" className="text-blue-600">
+              Login
+            </a>
+          </p>
+          {error && <p className="text-red-600 mt-2 text-center">{error}</p>}
+          {message && <p className="text-green-700 mt-2  ml-9">{message}</p>}
+        </form>
+      </div>
+    </>
   );
 }
